2329e2ce4e08725318b84856d80b34aaf950fa7d
João Rodrigues <joaoprodrigues@ua.pt>
2016-08-23 01:36:44 +0100

    Init 3.

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
 	'secret': 'jpf4eIsAwesome',
-	'database': 'mongodb://jpf4e:piranhas@uia-portal-3726.mongo.dbs.appsdeck.eu/uia-portal-3726'
+	'database': process.env.MONGO_URL
+	//'database': 'mongodb://jpf4e:piranhas@uia-portal-3726.mongo.dbs.appsdeck.eu/uia-portal-3726'
+	//'database': 'mongodb://localhost/uia-logging'
 }
