0df190359a9ee99e203a051d2f45040e7171b82a
João Rodrigues <joaoprodrigues@ua.pt>
2016-08-23 04:07:49 +0100

    Init 9.

--- public/js/constants.js
+++ public/js/constants.js
@@ -5,11 +5,11 @@ angular.module('starter', [])
 })
 
 .constant('API_ENDPOINT', {
-  url: 'http://127.0.0.1:8080/api'
+  url: 'https://uia-portal.scalingo.io/api'
   //  For a simulator use: url: 'http://127.0.0.1:8080/api'
 })
 
 .constant('HOME_ENDPOINT', {
-  url: 'http://127.0.0.1:8080/'
+  url: 'https://uia-portal.scalingo.io/'
   //  For a simulator use: url: 'http://127.0.0.1:8080/api'
 });
