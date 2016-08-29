9fac7b10902a741af9a1c9a037e32fe777e5423b
João Rodrigues <joaoprodrigues@ua.pt>
2016-08-29 05:39:24 +0100

    Fixed some bugs.

---
+++ public/js/controllers/SecSearchCtrl.js
@@ -0,0 +1,70 @@
+angular.module('SecSearchCtrl', []).controller('SecSearchController', function($http, $scope, $state, AuthService, AUTH_EVENTS, API_ENDPOINT) {
+
+	$scope.alert = null;
+
+	$scope.memberCatsEmpty = [true, true, true];
+	$scope.memberRoles = {};
+	$scope.memberName = "";
+	$scope.memberType = "";
+
+	$scope.currentState = $state;
+	$scope.logTitle = null;
+	$scope.logTitleSingular = null;
+	$scope.logIcon = null;
+
+	$scope.panel = {color: 'white', text: 'Input the username...', motto: ''};
+	$scope.search = {};
+
+	$scope.logout = function() {
+		AuthService.logout(false);
+		$state.go('sec-search');
+	};
+
+	$scope.closeAlert = function() {
+		$scope.alert = null;
+	};
+
+	$scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+		AuthService.logout(true);
+	});
+
+	$scope.$watch(function(){
+		return AuthService.isUserInfoReady();
+	}, function (newValue) {
+		if(newValue != null) {
+			$scope.memberCatsEmpty = AuthService.getMemberCatsEmpty();
+			$scope.memberRoles = AuthService.getMemberRoles();
+			$scope.memberName = AuthService.getMemberName();
+			$scope.memberType = AuthService.getMemberType();
+			if(newValue == false)
+				$scope.memberName = "";
+		}
+	}, true);
+
+	$scope.resetForm = function() {
+		$scope.search.username = null;
+		$scope.panel = {color: 'white', text: 'Input the username...', motto: ''};
+	}
+
+	$scope.secSearch = function() {
+		$http.post(API_ENDPOINT.url + '/secSearch', {username: $scope.search.username}).then(function(result) {
+			if (result.data.success) {
+				$scope.panel = result.data.panel;
+				console.log($scope.panel);
+			} else {
+				$scope.alert = {msg: result.data.msg, strong: "Failed to " + $scope.logTitle + "!"};
+			}
+		});
+	}
+
+	$scope.$watch('currentState.current.name', function (newValue) {
+		$scope.logTitle = "Security Search";
+		$scope.logTitleSingular = "Security Search";
+		$scope.logIcon = "eye-open";
+	}, true);
+
+	Date.prototype.addMinutes= function(h){
+		this.setMinutes(this.getMinutes()+h);
+		return this;
+	}
+});
